5d0b162edc89cf2bfefed0bc2904de39755025b3
agus grandon <agustina107.grandon@gmail.com>
2024-09-01 13:30:01 -0300

    agrege estilos en la seccion quienes somos, y acomode las fotos

--- src/components/QuienesSomos/QuienesSomos.jsx
+++ src/components/QuienesSomos/QuienesSomos.jsx
@@ -15,20 +15,20 @@ function QuienesSomos() {
           <h3 className="sobre">Sobre</h3>
           <h3 className="nosotros">Nosotros</h3>
           <br />
-          <p className="texto">
+          <p className="parrafo">
             Peluditos se centra en proporcionar un entorno seguro y amoroso para
             animales que han sido maltratados, abandonados o que necesitan un
             hogar temporal.
           </p>
           <br />
-          <p className="texto">
+          <p className="parrafo">
             Ofrecemos a los animales un cuidado inmediato, atención médica y un
             refugio seguro, además de facilitar la adopción de los animales a
             hogares permanentes asegurándose de que los futuros dueños sean
             aptos y comprometidos.
           </p>
           <br />
-          <p className="texto">
+          <p className="parrafo">
             Por otro lado, nos involucrandonos en la comunidad con apoyo de
             distintas personas del pais, ayudandonos a sostener este lugar que
             acoge con cariño a los animales.
@@ -41,25 +41,33 @@ function QuienesSomos() {
         </div>
       </div>
 
-    <div>
-      <h3>Nuestra mision</h3>
-    </div>
-      <div>
-        <p>
-          Peluditos nace en el año 2010 con el primer rescate en la provincia de
-          San Juan. Se trataba de un criadero de aproximadamente 15 perros en
-          estado de desnutrición y privados de su libertad.
-        </p>
-        <p>
-          Con la finalidad de salvarlos, nos reunimos los vecinos del Barrio
-          Maipu para poder colaborar a la causa y ayudar a los animales.
-        </p>
-        <p>
-          Desde allí, con el objetivo de poder encontrarles un hogar calido a
-          los perros rescatados, nos pusimos en marcha en difundir y poco a poco
-          organizar un espacio a aquellos animales que han pasado por la misma
-          situación.
-        </p>
+      <div className="contenedorDos">
+        <div className="texto">
+          <h3 className="sobre">Nuestra</h3>
+          <h3 className="nosotros">Mision</h3>
+          <br />
+          <p className="parrafo">
+            Peluditos nace en el año 2010 con el primer rescate en la provincia
+            de San Juan. Se trataba de un criadero de aproximadamente 15 perros
+            en estado de desnutrición y privados de su libertad.
+          </p>
+          <br />
+          <p className="parrafo">
+            Con la finalidad de salvarlos, nos reunimos los vecinos del Barrio
+            Maipu para poder colaborar a la causa y ayudar a los animales.
+          </p>
+          <br />
+          <p className="parrafo">
+            Desde allí, con el objetivo de poder encontrarles un hogar calido a
+            los perros rescatados, nos pusimos en marcha en difundir y poco a
+            poco organizar un espacio a aquellos animales que han pasado por la
+            misma situación.
+          </p>
+          <br />
+        </div>
+        <div className="imagenes">
+          <img className="foto" src={foto_3} />
+        </div>
       </div>
     </main>
   );
