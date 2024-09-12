4594732d55068ec0825dfe08960a14b6868abc41
Yanina Martin <yaninamicamartin@gmail.com>
2024-09-12 13:45:19 -0300

    mejoras home y colabora

--- src/components/Colabora/Colabora.jsx
+++ src/components/Colabora/Colabora.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Banner from "../../components/Colabora/img/imagen_banner.png";
+import Img_arriba from "../../components/Colabora/img/Rectangle_arriba.png";
+import Img_abajo from "../../components/Colabora/img/Rectangle_abajo.png";
+import "../Colabora/Colabora.css";
 
 function Colabora() {
   return (
-    <main>
-      <p>Hola</p>
+    <main className="colabora">
+      <img className="colabora_imgAbajo" src={Img_abajo} />
+
+      <img className="colabora_banner" src={Banner} />
+
+      <img className="colabora_imgArriba" src={Img_arriba} />
+      <p className="tituloColabora">¿De qué manera?</p>
     </main>
   );
 }
