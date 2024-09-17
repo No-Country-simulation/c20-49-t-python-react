8c9ba4da3573d6c4c4229b3ac8d7e1a3e8b02349
agus grandon <agustina107.grandon@gmail.com>
2024-09-17 17:33:22 -0300

    agrege la misma info en la seccion gatitos

--- src/components/Adopta_gatos/Adopta_gatos.jsx
+++ src/components/Adopta_gatos/Adopta_gatos.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Adopta_gatos.css";
 import rectangulo_1 from "../QuienesSomos/img/rectangulo_1.png";
 import rectangulo_2 from "../QuienesSomos/img/rectangulo_2.png";
-
+import perrito_1 from "../Adopta_perros/img/perrito_1.png";
+import perrito_2 from "../Adopta_perros/img/perrito_2.png";
+import perrito_3 from "../Adopta_perros/img/perrito_3.png";
+import perrito_4 from "../Adopta_perros/img/perrito_4.png";
+import rectangulo_blanco_pie_de_pagina from "../Adopta_perros/img/rectangulo_blanco_pie_de_pagina.png"; 
 
 function Adopta_gatos() {
   return (
@@ -14,7 +18,69 @@ function Adopta_gatos() {
       <img className="rectanguloDos" src={rectangulo_2} />
       <h3 className="tituloAdoptaPerros">Encontrá a tu mejor <br /> amigo <span className="perruno">gatuno</span></h3>
 
-      /* carrusel */
+      <div className="imagen_fondo_perritos">
+      <div className="col-xs-6 col-sm-10 col-md-12 col-lg-12 d-flex flex-wrap justify-content-around">
+        
+        <div className="cardItem" style={{ width: "17rem", height: "25rem" }}>
+          <img src={perrito_1} className="card-img-top" alt="foto"/>
+            <div className="card-body">
+            <h5 className="tituloCinco text-center">Canela</h5>
+            </div>
+        </div>
+        
+        <div className="cardItem" style={{ width: "17rem", height: "25rem" }}>
+          <img src={perrito_2} className="card-img-top" alt="foto"/>
+            <div className="card-body">
+              <h5 className="tituloCinco text-center">Pocho</h5>
+            </div>
+        </div>
+
+        <div className="cardItem" style={{ width: "17rem", height: "25rem" }}>
+          <img src={perrito_3} className="card-img-top" alt="foto"/>
+            <div className="card-body">
+              <h5 className="tituloCinco text-center">Luli</h5>
+            </div>
+        </div>
+
+        <div className="cardItem" style={{ width: "17rem", height: "25rem" }}>
+          <img src={perrito_4} className="card-img-top" alt="foto"/>
+            <div className="card-body">
+              <h5 className="tituloCinco text-center">Tomy</h5>
+            </div>
+        </div>
+
+        <div className="cardItem" style={{ width: "17rem", height: "25rem" }}>
+          <img src={perrito_1} className="card-img-top" alt="foto"/>
+            <div className="card-body">
+              <h5 className="tituloCinco text-center">Taylor</h5>
+            </div>
+        </div>
+        
+        <div className="cardItem" style={{ width: "17rem", height: "25rem" }}>
+          <img src={perrito_2} className="card-img-top" alt="foto"/>
+            <div className="card-body">
+              <h5 className="tituloCinco text-center">Selena</h5>
+            </div>
+        </div>
+
+        <div className="cardItem" style={{ width: "17rem", height: "25rem" }}>
+          <img src={perrito_3} className="card-img-top" alt="foto"/>
+            <div className="card-body">
+              <h5 className="tituloCinco text-center">Olivia</h5>
+            </div>
+        </div>
+
+        <div className="cardItem" style={{ width: "17rem", height: "25rem" }}>
+          <img src={perrito_4} className="card-img-top" alt="foto"/>
+            <div className="card-body">
+              <h5 className="tituloCinco text-center">Meredith</h5>
+            </div>
+        </div>
+        <img src={rectangulo_blanco_pie_de_pagina} alt="" />
+
+      </div>
+      </div>
+      
       
     </main>
   );
