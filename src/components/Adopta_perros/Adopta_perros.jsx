945a39dfc461fb1a8d673fc1c25fa20db65fd454
agus grandon <agustina107.grandon@gmail.com>
2024-09-17 15:49:54 -0300

    cree el archivo y las carpetas de la seccion adopta

---
+++ src/components/Adopta_perros/Adopta_perros.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Adopta_perros.css";
+import { Link } from "react-router-dom";
+
+
+function Adopta_perros() {
+  return (
+    <main>
+
+    </main>
+  );
+}
+
+export default Adopta_perros;
