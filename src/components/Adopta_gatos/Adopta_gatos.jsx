df61d39a7a516ef1e807357ac2181d5e80d775cd
agus grandon <agustina107.grandon@gmail.com>
2024-09-17 16:29:23 -0300

    agrege el titulo de la pagina y la foto de fondo

--- src/components/Adopta_gatos/Adopta_gatos.jsx
+++ src/components/Adopta_gatos/Adopta_gatos.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./Adopta_gatos.css";
+import rectangulo_1 from "../QuienesSomos/img/rectangulo_1.png";
+import rectangulo_2 from "../QuienesSomos/img/rectangulo_2.png";
 
 
 function Adopta_gatos() {
   return (
     <main>
-      <h2 className="tituloAdoptaPerros">Encontrá a tu mejor <br /> amigo <span className="perruno">perruno</span></h2>
+      <img className="rectanguloUno" src={rectangulo_1} />
+      <div className="imagenDeFondoGatos">
+        <h2 className="tituloGatos">Busca a tu compañia preferida</h2>
+      </div>
+      <img className="rectanguloDos" src={rectangulo_2} />
+      <h3 className="tituloAdoptaPerros">Encontrá a tu mejor <br /> amigo <span className="perruno">perruno</span></h3>
     </main>
   );
 }
