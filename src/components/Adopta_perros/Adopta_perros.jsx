85a7dd9486b3a396b31b7b3f6675c25955fdd11f
agus grandon <agustina107.grandon@gmail.com>
2024-09-17 20:26:52 -0300

    agrege un titulo de prubea

--- src/components/Adopta_perros/Adopta_perros.jsx
+++ src/components/Adopta_perros/Adopta_perros.jsx
@@ -15,6 +15,7 @@ function Adopta_perros() {
       <img className="rectanguloUno" src={rectangulo_1} />
       <div className="imagenDeFondoGatos">
         <h2 className="tituloGatos">Busca a tu compañia preferida</h2>
+        <h3>titulo de prueba</h3>
       </div>
       <img className="rectanguloDos" src={rectangulo_2} />
 
