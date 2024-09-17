7a18832f5b5c563478f596769c110e8e04175519
agus grandon <agustina107.grandon@gmail.com>
2024-09-17 15:51:06 -0300

    cree la carpeta de adopcion de los gatos

---
+++ src/components/Adopta_gatos/Adopta_gatos.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Adopta_gatos.css";
+import { Link } from "react-router-dom";
+
+
+function Adopta_gatos() {
+  return (
+    <main>
+
+    </main>
+  );
+}
+
+export default Adopta_gatos;
