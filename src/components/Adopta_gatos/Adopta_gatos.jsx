9e1d2a63c35b294b9656ff84f1656c27b7b60a53
agus grandon <agustina107.grandon@gmail.com>
2024-09-18 16:41:01 -0300

    agrege las fotos de los gatos

--- src/components/Adopta_gatos/Adopta_gatos.jsx
+++ src/components/Adopta_gatos/Adopta_gatos.jsx
@@ -6,6 +6,10 @@ import gatito_1 from "./img/gatito_1.png";
 import gatito_2 from "./img/gatito_2.png";
 import gatito_3 from "./img/gatito_3.png";
 import gatito_4 from "./img/gatito_4.png";
+import gatito_5 from "./img/gatito_5.png";
+import gatito_6 from "./img/gatito_6.png";
+import gatito_7 from "./img/gatito_7.png";
+import gatito_8 from "./img/gatito_8.png";
 import rectangulo_blanco_pie_de_pagina from "../Adopta_perros/img/rectangulo_blanco_pie_de_pagina.png"; 
 
 function Adopta_gatos() {
@@ -21,59 +25,67 @@ function Adopta_gatos() {
       <div className="imagen_fondo_perritos">
       <div className="col-xs-6 col-sm-10 col-md-12 col-lg-12 d-flex flex-wrap justify-content-around">
         
-        <div className="cardItem" style={{ width: "17rem", height: "23rem" }}>
+        <div className="cardItem" style={{ width: "17rem", height: "30rem" }}>
           <img src={gatito_1} className="card-img-top" alt="foto"/>
             <div className="card-body">
             <h5 className="tituloCinco text-center">LOLA</h5>
+            <p className="parrafoAdoptaPerros">Se encuentra castrado, vacunado, tiene 10 años</p>
             </div>
         </div>
         
-        <div className="cardItem" style={{ width: "17rem", height: "23rem" }}>
+        <div className="cardItem" style={{ width: "17rem", height: "30rem" }}>
           <img src={gatito_2} className="card-img-top" alt="foto"/>
             <div className="card-body">
               <h5 className="tituloCinco text-center">GALA</h5>
+              <p className="parrafoAdoptaPerros">Se encuentra castrado, vacunado, tiene 10 años</p>
             </div>
         </div>
 
-        <div className="cardItem" style={{ width: "17rem", height: "23rem" }}>
+        <div className="cardItem" style={{ width: "17rem", height: "30rem" }}>
           <img src={gatito_3} className="card-img-top" alt="foto"/>
             <div className="card-body">
               <h5 className="tituloCinco text-center">LUIGI</h5>
+              <p className="parrafoAdoptaPerros">Se encuentra castrado, vacunado, tiene 10 años</p>
             </div>
         </div>
 
-        <div className="cardItem" style={{ width: "17rem", height: "23rem" }}>
+        <div className="cardItem" style={{ width: "17rem", height: "30rem" }}>
           <img src={gatito_4} className="card-img-top" alt="foto"/>
             <div className="card-body">
               <h5 className="tituloCinco text-center">MARIO</h5>
+              <p className="parrafoAdoptaPerros">Se encuentra castrado, vacunado, tiene 10 años</p>
             </div>
         </div>
 
-        <div className="cardItem" style={{ width: "17rem", height: "23rem" }}>
-          <img src={gatito_1} className="card-img-top" alt="foto"/>
+        <div className="cardItem" style={{ width: "17rem", height: "30rem" }}>
+          <img src={gatito_5} className="card-img-top" alt="foto"/>
             <div className="card-body">
               <h5 className="tituloCinco text-center">THEODORA</h5>
+              <p className="parrafoAdoptaPerros">Se encuentra castrado, vacunado, tiene 10 años</p>
             </div>
         </div>
         
-        <div className="cardItem" style={{ width: "17rem", height: "23rem" }}>
-          <img src={gatito_2} className="card-img-top" alt="foto"/>
+        <div className="cardItem" style={{ width: "17rem", height: "30rem" }}>
+          <img src={gatito_6} className="card-img-top" alt="foto"/>
             <div className="card-body">
               <h5 className="tituloCinco text-center">BIGOTES</h5>
+              <p className="parrafoAdoptaPerros">Se encuentra castrado, vacunado, tiene 10 años</p>
             </div>
         </div>
 
-        <div className="cardItem" style={{ width: "17rem", height: "23rem" }}>
-          <img src={gatito_3} className="card-img-top" alt="foto"/>
+        <div className="cardItem" style={{ width: "17rem", height: "30rem" }}>
+          <img src={gatito_7} className="card-img-top" alt="foto"/>
             <div className="card-body">
               <h5 className="tituloCinco text-center">SIMBA</h5>
+              <p className="parrafoAdoptaPerros">Se encuentra castrado, vacunado, tiene 10 años</p>
             </div>
         </div>
 
-        <div className="cardItem" style={{ width: "17rem", height: "23rem" }}>
-          <img src={gatito_4} className="card-img-top" alt="foto"/>
+        <div className="cardItem" style={{ width: "17rem", height: "30rem" }}>
+          <img src={gatito_8} className="card-img-top" alt="foto"/>
             <div className="card-body">
               <h5 className="tituloCinco text-center">NUBE</h5>
+              <p className="parrafoAdoptaPerros">Se encuentra castrado, vacunado, tiene 10 años</p>
             </div>
         </div>
       </div>
